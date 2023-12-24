2f3b7e9c77ea21ac8bd817db3d8d41312a833955
go2023 <tetyanago2018@gmail.com>
2023-12-23 18:17:21 -0800

    react stuff preparation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Root from './pages/root';
+import WithButton from './pages/withButton'
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+    },
+    {
+        path: "/with-button",
+        element: <WithButton/>,
+    },
+    {
+        path: "/height-calc",
+        element: 'sdfsdwith button',
+    },
+    {
+        path: "/infinit-lib",
+        element: '/infinit-lib',
+    },
+    {
+        path: "/intersection",
+        element: '/intersection',
+    },
+    {
+        path: "/virtual-window",
+        element: '/virtual-window',
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
 )
